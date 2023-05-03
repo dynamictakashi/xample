a9b0615b5b74a5c1e819fa526da28f371808dbd4
mew <wawe@Macintosh.local>
2023-05-03 12:30:30 +0900

    ナビゲーションの変更

--- app.py
+++ app.py
@@ -1,41 +1,35 @@
 from flask import Flask, render_template, request
-#乱数生成用モジュール
+# 乱数生成用モジュール
 import random
 
+
 app = Flask(__name__)
+app.debug = True
+app.config['DEBUG'] = True
+
 
 @app.route('/')
 def index():
-    return render_template('base.html')
-
-@app.route('/home')
-def home():
     return render_template('home.html')
+
 
 @app.route('/triangle', methods=['GET', 'POST'])
 def janken():
+    you = ""
+    me = ""
     result = ""
-    selected_hand = ""
-    computer_hand = ""
     if request.method == 'POST':
-        # HTMLフォームから送信されたデータを取得
-        selected_hand = request.form['hand']
-        # コンピュータの手をランダムに決定
-        hands = ["グー", "チョキ", "パー"]
-        #リスト+randintを用いてhandsの中身をランダム出力できるようにします。
-        computer_hand = hands[random.randint(0,2)]
-        
-        # 勝敗の判定 1=チョキ, 2=パー,0=グー
-        if selected_hand == "グー" and computer_hand == "チョキ":
-            result = "勝ち"
-        elif selected_hand == "チョキ" and computer_hand == "パー":
-            result = "勝ち"
-        elif selected_hand == "パー" and computer_hand == "グー":
-            result = "勝ち"
-        elif selected_hand == computer_hand:
-            result = "引き分け"
+        hand = ['グー', 'チョキ', 'パー']
+        me = request.form['hand']
+        you = hand[random.randint(0, 2)]
+        if you == me:
+            result = 'あいこ'
+        elif me == 'グー' and you == 'チョキ':
+            result = '勝ち'
+        elif me == 'チョキ' and you == 'パー':
+            result = '勝ち'
+        elif me == 'パー' and you == 'グー':
+            result = '勝ち'
         else:
-            result = "負け"
-    #レンダーテンプレートに表示したい変数を渡してあげましょう。
-    return render_template('triangle.html', result=result,myhand=selected_hand,cphand=computer_hand)
-
+            result = '負け'
+    return render_template('triangle.html', result=result, myhand=me, cphand=you)
