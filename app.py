990d4a9745223f33c9fcbefa4d6bc3e85fb6b23b
mew <wawe@Macintosh.local>
2023-05-03 14:22:40 +0900

    DB追加

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template, request
 # 乱数生成用モジュール
 import random
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.debug = True
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+db=SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route('/')
 def index():
@@ -33,3 +42,10 @@ def janken():
         else:
             result = '負け'
     return render_template('triangle.html', result=result, myhand=me, cphand=you)
+
+@app.route('/add_db')
+def add_db():
+    new_db = User(username='example', email='test@example.com')
+    db.session.add(new_db)
+    db.session.commit()
+    return '追加完了'
