21e4627e6624ec7ab1b9d2a7ba546781af52a6b3
mew <wawe@Macintosh.local>
2023-05-04 12:34:54 +0900

    ジャンケン縮小

--- app.py
+++ app.py
@@ -31,19 +31,15 @@ def janken():
     result = ""
     if request.method == 'POST':
         hand = ['グー', 'チョキ', 'パー']
-        me = request.form['hand']
-        you = hand[random.randint(0, 2)]
-        if you == me:
+        me = hand.index(request.form['hand'])
+        you = random.randint(0, 2)
+        if (me-you) % 3 == 0:
             result = 'あいこ'
-        elif me == 'グー' and you == 'チョキ':
-            result = '勝ち'
-        elif me == 'チョキ' and you == 'パー':
-            result = '勝ち'
-        elif me == 'パー' and you == 'グー':
+        elif (me-you) % 3 == 1:
             result = '勝ち'
         else:
             result = '負け'
-    return render_template('triangle.html', result=result, myhand=me, cphand=you)
+    return render_template('triangle.html', myhand=me, cphand=you, result=result, xhand=hand)
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -59,17 +55,44 @@ def add_db():
         return render_template('signup.html')
 
 
-@app.route('/find', methods=['GET', 'POST'])    #method"s"にする
+@app.route('/find', methods=['GET', 'POST'])  # method"s"にする
 def db_find():
-    result = ""     #最初に定義しないと動かない
-    username = ""   #最初に定義しないと動かない
-    email = ""      #最初に定義しないと動かない
+    result = ""  # 最初に定義しないと動かない
+    username = ""  # 最初に定義しないと動かない
+    email = ""  # 最初に定義しないと動かない
     if request.method == 'POST':
-        id = request.form.get('id') #HTMLと連携、HTMLから数値取得
-        found = User.query.get(id)  #変数からDB取得
-        username = found.username   #変数からDBカラムを持ってくる
-        email = found.email         #同じくカラム持ってくる
-        result = 'enable'           #ダミー用関数
+        id = request.form.get('id')  # HTMLと連携、HTMLから数値取得
+        found = User.query.get(id)  # 変数からDB取得
+        username = found.username  # 変数からDBカラムを持ってくる
+        email = found.email  # 同じくカラム持ってくる
+        result = 'enable'  # ダミー用関数
         return render_template('find.html', result=result, username=username, email=email)
     else:
         return render_template('find.html')
+
+
+@app.route('/sandbox', methods=['GET', 'POST'])
+def jankeeen():
+    if request.method == 'POST':
+        opo = ""
+        me = ""
+        result = ""
+        hand = ['グー', 'チョキ', 'パー']
+        me = request.form.get('hand')
+        opo = hand[random.randint(0, 2)]
+        if opo == me:
+            result = 'あいこ'
+        elif me == 'グー' and opo == 'チョキ':
+            result = '勝ち'
+        elif me == 'チョキ' and opo == 'パー':
+            result = '勝ち'
+        elif me == 'パー' and opo == 'グー':
+            result = '勝ち'
+        else:
+            result = '負け'
+        print(result)
+        print(me)
+        print(opo)
+        return render_template('sandbox.html')
+    else:
+        return render_template('sandbox.html')
